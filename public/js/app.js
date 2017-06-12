ac0668c693b4b0ffe7a068a0e0003ce80affe006
Edith Mandujano <edithmandujano001@gmail.com>
2017-06-12 16:23:18 -0500

    no muestra

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,39 @@
-var cargarPagina = function() {
-  cargarPokemon();
-  //  $(document).on("click", ".pokemon", mostrarDetallePokemon);
-};
+// Funcion para cargar la pagina
+var cargarPagina=function(){
+  cargarPokemones();
+}
 
-var cargarPokemon = function() {
-  var url = "http://pokeapi.co/api/v2/pokemon/";
-  $.getJSON(url, function(response) {
-    //console.log(response); nos da la lista de los primeros 20 pokemones
-    var pokemones = response.results;
-    mostrarPokemones(pokemones);
+// LLamar lista pokemones
+var cargarPokemones=function(){
+  var url="http://pokeapi.co/api/v2/pokemon/";
+  $.getJSON(url, function (response) {
+    // De la respuesta del servidor ocupamos los Resultados
+    var pokemones=response.results;
+    mostrarPokemones (pokemones);
   });
 };
 
-
-var mostrarPokemones = function(pokemones) {
-  var $ul = $("#listaPokemones");
-  pokemones.forEach(function(pokemon) {
-    var $li = $("<li />");
-    $li.addClass("pokemon");
-    $li.text(pokemon.name);
-    $ul.append($li);
-
-    //console.log ($li);
-  });
+var mostrarPokemones=function (pokemones) {
 
 
+  var $section=$("#listaPokemones");
+  pokemones.forEach(function (pokemon) {
+  /*var $div=$("<div />");
+  $div.addClass(".pokemon");
+  $div.text(pokemon.name);
+  $section.append($div);*/
+  $section.html(plantilla.replace("__nombre__", pokemon.name));
 
-
+});
 };
 
+
+var plantilla= '<div>' +
+'<img src="public/assets/img/"__nombre__".png">' +
+'<h5>__nombre__</h5>' +
+'</div>';
+
+
+
+// Para cargar el documento
 $(document).ready(cargarPagina);
